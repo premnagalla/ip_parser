9dbb6cae6d25c1104261fa6adc740f88139cb9b6
Prem <prem.nagalla@gmail.com>
2013-12-10 01:50:15 +0530

    Initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,57 @@
+class WelcomeController < ApplicationController
+
+  def index
+  end
+
+  def parse_csv
+    # raise params.to_yaml
+    require 'csv'
+    input_file = params[:input_file]
+
+    # To write to a file
+    output_file_path = "#{Rails.root}/tmp/ips_output_data.csv"
+
+    # Delete previous generated file, if there is any
+    File.delete(output_file_path) if File.file?(output_file_path)
+
+    # Open a new file and write the required data fetched
+    File.open(output_file_path, "w") do |csv|
+      header_attributes = ["Given country","IP Address","City","Region","Country"]
+      header_string = header_attributes.join(',')
+      csv << header_string
+      csv << "\n"
+
+      # Parse & write to file
+      CSV.parse(input_file.read, :headers => false) do|row|
+        begin
+          # row.headers.each{ |cell| row[cell] = row[cell].to_s.strip }
+          given_country = row[3]
+          given_ip = row[4]
+          result = Geocoder.search(given_ip)
+          if result[0].present? && result[0].data.present?
+            op_data_string = "#{given_country},#{given_ip},#{result[0].data['city']},#{result[0].data['region_name']},#{result[0].data['country_name']},Valid IP"
+            csv << op_data_string
+            csv << "\n"      
+          else
+            op_data_string = "#{given_country},#{given_ip},,,,InValid IP"
+            csv << op_data_string
+            csv << "\n"      
+          end
+        rescue Exception => e
+          p "failed for record: #{row}"
+          p "Exception ---------: #{e.message}"
+          # op_data_string = "#{given_country},#{given_ip},,,,InValid IP"
+          # csv << op_data_string
+          # csv << "\n"                
+        end
+      end
+      send_file output_file_path, :type=>"application/csv", :x_sendfile=>true      
+    end
+
+    respond_to do|format|
+      format.html{}
+      format.js{}    
+    end
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IpParser::Application.config.secret_token = 'e6886d66cd3187b297afc783f6080c2d2476733f77569c6948f7439d40566676c2a69735ca745a06daf5c932ae2d2e9068b11dc96a6e4c931d1f43c80cd26413'
